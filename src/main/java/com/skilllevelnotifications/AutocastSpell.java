0514d9e061fc9122a41c1ddc524084e321cae2aa
jcarbelbide <jcarbelbide@gmail.com>
2021-12-30 20:41:15 -0800

    Working to detect when magic level has dropped below autocast spell level. Also fixed AUTOCAST_SPELL to be Varbit instead of VarClientInt.

---
+++ src/main/java/com/skilllevelnotifications/AutocastSpell.java
@@ -0,0 +1,69 @@
+package net.runelite.client.plugins.skilllevelnotifications.src.main.java.com.skilllevelnotifications;
+
+import com.google.common.collect.ImmutableMap;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import net.runelite.api.Varbits;
+
+import java.util.Map;
+
+//TODO: Add Magic Dart, Crumble Undead, Flames of Zamorak, Saradomin Strike, Claws of Guthix
+
+@RequiredArgsConstructor
+@Getter
+enum AutocastSpell
+{
+    NOSPELL("No spell selected", Varbits.AUTOCAST_SPELL, -1, 0),
+
+    WIND_STRIKE("Wind Strike", Varbits.AUTOCAST_SPELL, 1 , 1),
+    WATER_STRIKE("Water Strike", Varbits.AUTOCAST_SPELL, 5 , 2),
+    EARTH_STRIKE("Earth Strike", Varbits.AUTOCAST_SPELL, 9 , 3),
+    FIRE_STRIKE("Fire Strike", Varbits.AUTOCAST_SPELL, 13, 4),
+
+    WIND_BOLT("Wind Bolt", Varbits.AUTOCAST_SPELL, 17, 5),
+    WATER_BOLT("Water Bolt", Varbits.AUTOCAST_SPELL, 23, 6),
+    EARTH_BOLT("Earth Bolt", Varbits.AUTOCAST_SPELL, 29, 7),
+    FIRE_BOLT("Fire Bolt", Varbits.AUTOCAST_SPELL, 35, 8),
+
+    WIND_BLAST("Wind Blast", Varbits.AUTOCAST_SPELL, 41,  9),
+    WATER_BLAST("Water Blast", Varbits.AUTOCAST_SPELL, 47, 10),
+    EARTH_BLAST("Earth Blast", Varbits.AUTOCAST_SPELL, 53, 11),
+    FIRE_BLAST("Fire Blast", Varbits.AUTOCAST_SPELL, 59, 12),
+
+    WIND_WAVE("Wind Wave", Varbits.AUTOCAST_SPELL, 62, 13),
+    WATER_WAVE("Water Wave", Varbits.AUTOCAST_SPELL, 65, 14),
+    EARTH_WAVE("Earth Wave", Varbits.AUTOCAST_SPELL, 70, 15),
+    FIRE_WAVE("Fire Wave", Varbits.AUTOCAST_SPELL, 75, 16),
+
+    IBANS_BLAST("Iban's Blast", Varbits.AUTOCAST_SPELL, 50, 47),
+
+    WIND_SURGE("Wind Surge", Varbits.AUTOCAST_SPELL, 81, 48),
+    WATER_SURGE("Water Surge", Varbits.AUTOCAST_SPELL, 85, 49),
+    EARTH_SURGE("Earth Surge", Varbits.AUTOCAST_SPELL, 90, 50),
+    FIRE_SURGE("Fire Surge", Varbits.AUTOCAST_SPELL, 95, 51);
+
+    @Getter
+    private final String name;
+    @Getter
+    private final Varbits autocastSpellVarClientInt;
+    @Getter
+    private final int levelRequirement;
+    @Getter
+    private final int autocastSpellID;
+
+    private static final Map<Integer, AutocastSpell> AUTOCAST_SPELLS_MAP;
+
+    static
+    {
+        ImmutableMap.Builder<Integer, AutocastSpell> builder = new ImmutableMap.Builder<>();
+
+        for (AutocastSpell spell : values())
+        {
+            builder.put(spell.getAutocastSpellID(), spell);
+        }
+
+        AUTOCAST_SPELLS_MAP = builder.build();
+    }
+
+    public static AutocastSpell getAutocastSpell(int autocastSpellID) { return AUTOCAST_SPELLS_MAP.get(autocastSpellID); }
+}
--- src/main/java/com/skilllevelnotifications/SkillLevelNotificationsPlugin.java
+++ src/main/java/com/skilllevelnotifications/SkillLevelNotificationsPlugin.java
@@ -2,20 +2,20 @@ package net.runelite.client.plugins.skilllevelnotifications.src.main.java.com.sk
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
+
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
+import net.runelite.api.*;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.StatChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-import net.runelite.api.Skill;
+import net.runelite.client.ui.overlay.infobox.InfoBoxManager;
 
 /*
-TODO: When skills lowered, check to see if the skill lowered enough that it would unequip the autocasted spell
+TODO: Add overlay that displays skill name and then flashes when skill too low
 TODO: Add remaining spells to AutocastSpells enum.
 TODO: On startup, should initialize, getting the autocasted spell and current mage level.
  */
@@ -28,16 +28,23 @@ TODO: On startup, should initialize, getting the autocasted spell and current ma
 )
 public class SkillLevelNotificationsPlugin extends Plugin
 {
+	@Getter
+	private boolean magicLevelMeetsSpellReq;
+
 	@Inject
 	private Client client;
 
 	@Inject
 	private SkillLevelNotificationsConfig config;
 
+	@Inject
+	private InfoBoxManager infoBoxManager;
+
 	@Override
 	protected void startUp() throws Exception
 	{
 		log.info("SkillLevelNotificationsPlugin started!");
+//		infoBoxManager.addInfoBox();
 	}
 
 	@Override
@@ -47,28 +54,31 @@ public class SkillLevelNotificationsPlugin extends Plugin
 	}
 
 	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
-		}
-	}
-
-	@Subscribe
 	public void onStatChanged(StatChanged event)
 	{
 		Skill skill = event.getSkill();
 		int xp = event.getXp();
-		int level = event.getLevel();
+		int statLevel = event.getLevel();
 		int boostedLevel = event.getBoostedLevel();
 
 		if (skill.getName().equals(Skill.MAGIC.getName()))
 		{
-			System.out.println("Skill: " + skill.getName() + ", xp: " + String.valueOf(xp) + ", level: " + String.valueOf(level) + ", boostedLevel: " + String.valueOf(boostedLevel));
-			System.out.println("Was Magic");
-
-
+			// Now need to check if new boostedLevel is still high enough for the autocast spell
+			int autocastSpellID = client.getVar(Varbits.AUTOCAST_SPELL);
+			AutocastSpell autocastSpell = AutocastSpell.getAutocastSpell(autocastSpellID);
+			if (boostedLevel < autocastSpell.getLevelRequirement())
+			{
+				magicLevelMeetsSpellReq = false;
+				System.out.println("Level too low to autocast.");
+			}
+			else
+			{
+				magicLevelMeetsSpellReq = true;
+				System.out.println("Magic level high enough for autocast");
+			}
+			// For debug
+			System.out.println("Skill: " + skill.getName() + ", xp: " + String.valueOf(xp) + ", statLevel: " + String.valueOf(statLevel) + ", boostedLevel: " + String.valueOf(boostedLevel));
+			System.out.println("Level Req: " + String.valueOf(autocastSpell.getLevelRequirement()) + " Name: " + String.valueOf(autocastSpell.getName()) + " Varbit: " + String.valueOf(autocastSpell.getAutocastSpellVarClientInt()) + " ID: " + String.valueOf(autocastSpell.getAutocastSpellID()) );
 		}
 	}
 
